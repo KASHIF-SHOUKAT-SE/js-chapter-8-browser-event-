c8a94ef6b964f1643abd6ad553805f134cca72b4
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-05-02 14:16:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// let a = document.getElementsByClassName("conatainer")[0];
+// a.onclick = () => {
+//     let b = document.getElementsByClassName("conatainer")[0];
+//     b.innerHTML= "hello world";
+// } removeit
